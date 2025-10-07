eab88a11576f5635d0f23cf7b8e4726efe6b0933
pat123 <phollakrit@hotmail.com>
2025-10-07 15:00:23 +0700

    add update and delete

--- local/src/db-nav.js
+++ local/src/db-nav.js
@@ -1,7 +1,8 @@
 import React from 'react'
 import DBCreate from './db-create'
 import DBRead from './db-read'
-
+import DBUpdate from './db-update'
+import DBDelete from './db-delete'
 import DBPaginate from './db-paginate'
 import DBSearch from './db-search'
 
@@ -13,14 +14,16 @@ export default function DBNav() {
                 <div style={{margin: '30px 50px'}}>
                     <a href="/db/create">เพิ่มข้อมูล</a> <br/>
                     <a href="/db/read">แสดงข้อมูล</a> <br/>
-                    
+                    <a href="/db/update">แก้ไขข้อมูล</a> <br/>
+                    <a href="/db/delete">ลบข้อมูล</a> <br/>
                     <a href="/db/paginate">แบ่งเพจ</a> <br/>
                     <a href="/db/search">Workshop: ค้นหาข้อมูล</a> <br/>
                 </div>
             )
         case '/db/create':  return <DBCreate/>
         case '/db/read': return <DBRead/>
-
+        case '/db/update': return <DBUpdate/>
+        case '/db/delete': return <DBDelete/>
         case '/db/paginate': return <DBPaginate/>
         case '/db/search': return <DBSearch/>
         default: window.location.href = '/db'
--- server/index.js
+++ server/index.js
@@ -34,7 +34,50 @@ app.get('/api/db/read', (request, response) => {
     })
 })
 
+app.post('/api/db/update', (request, response) => {
+    let form = request.body
+    let data = {
+        name: form.name || '', 
+        price: form.price || 0, 
+        detail: form.detail || '',
+        date_added: new Date(Date.parse(form.date_added)) || new Date()
+    }
 
+	Product
+	.findByIdAndUpdate(form._id, data, { useFindAndModify: false })
+	.exec(err => {
+        if (err) {
+            response.json({error: err})
+            return
+        }
+    })	
+    	
+    //หลังการอัปเดต ก็อ่านข้อมูลอีกครั้ง แล้วส่งไปแสดงผลที่ฝั่งโลคอลแทนข้อมูลเดิม
+    Product
+    .find()     
+    .exec((err, docs) => {
+        response.json(docs)
+    })
+})
+
+app.post('/api/db/delete', (request, response) => {
+    let _id = request.body._id
+
+	Product
+	.findByIdAndDelete(_id, { useFindAndModify: false })
+	.exec(err => {
+        if (err) {
+            response.json({error: err})
+            return
+        }
+    })		
+
+    Product
+    .find()     
+    .exec((err, docs) => {
+        response.json(docs)
+    })
+})
 
 app.get('/api/db/paginate', (request, response) => {
 	let options = {
